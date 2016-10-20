ffc7912904fc64d050d78f1fd11a751a44df8541
i15011 <i15011@WIN10A-I15011>
2016-10-20 14:11:53 +0200

    Projektdateien hinzufügen.

---
+++ App2Night/Views/FensterVeranstaltungAnzeigen.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace App2Night.Views
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class FensterVeranstaltungAnzeigen : Page
+    {
+        public FensterVeranstaltungAnzeigen()
+        {
+            //DateTimeOffset aktuellesJahr = DateTime.Today.AddYears(0);
+            //DateTimeOffset aktuellesJahrPlusEins = DateTime.Today.AddYears(1);
+
+            this.InitializeComponent();
+            //this.DatePickerVeranstSuche.Date = DateTime.Today;
+            //this.DatePickerVeranstSuche.MinYear = aktuellesJahr;
+            //this.DatePickerVeranstSuche.MaxYear = aktuellesJahrPlusEins;
+        }
+    }
+}
