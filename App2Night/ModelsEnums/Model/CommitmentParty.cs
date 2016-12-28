0aab82cfd2a30da116668e74edae748338834392
capricorn776 <manuela.leopold@gmx.de>
2016-12-28 21:53:08 +0100

    Buttons für Teilnehmen und Voten hinzufügen, Backendkommunikation für das Teilnehmen

---
+++ App2Night/ModelsEnums/Model/CommitmentParty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App2Night.ModelsEnums.Model;
+
+namespace App2Night.ModelsEnums.Model
+{
+    public class CommitmentParty
+    {
+        public CommitmentParty CommitmentState { get; set; }
+        public Party Party { get; set; }
+    }
+}
